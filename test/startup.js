b9451a2f8edacaae0348135babd329aa63b8d8e9
edloidas <edloidas@gmail.com>
2016-05-14 12:27:55 +0300

    :white_check_mark: Add CI support and tests.
    Added basic `package.json`.
    Added Travis CI and AppVeyor configs.
    Added dummy test.
    Added coverage report script.

---
+++ test/startup.js
@@ -0,0 +1,10 @@
+const should = require( 'should' );
+
+// Just a dummy test for now
+describe( 'server', () => {
+  let msg = 'should start without errors';
+	it( msg, ( done ) => {
+    // should expression here
+		done();
+	});
+});
