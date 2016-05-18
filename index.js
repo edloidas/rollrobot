ccf065e93c5c3673719f5ddb7e020278d9928e18
edloidas <edloidas@gmail.com>
2016-05-18 19:33:52 +0300

    Fix the response dest from `msg.from` User to `msg.chat` Chat.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const TelegramBot = require( 'node-telegram-bot-api' );
 const CONFIG = require( './src/config' );
 const message = require( './src/message' );
-const dice = require( './src/dice' );
 
 const { token, settings, telegram } = CONFIG;
 const URL = `${ telegram.host }:${ telegram.port }/bot${ token }`;
@@ -11,13 +10,14 @@ bot.setWebHook( URL );
 
 // /start
 bot.onText( message.type.start.regexp, ( msg ) => {
-  const fromId = msg.from.id;
+  const id = msg.chat.id;
   const { resp, options } = message.type.start;
-  bot.sendMessage( fromId, resp, options );
+  bot.sendMessage( id, resp, options );
 });
 
 function commonRollHandler( type, msg, match ) {
-  const { id, username } = msg.from;
+  const id = msg.chat.id;
+  const username = msg.from ? msg.from.username : msg.chat.username;
   try {
     const { resp, options } = message.getMessageBody( type, username, match[ 2 ]);
     bot.sendMessage( id, resp, options );
