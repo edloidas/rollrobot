9876c67fc75068930407ee685ee79a2f3c0825aa
Mikita Taukachou <edloidas@gmail.com>
2022-12-08 08:53:07 +0300

    Upgrade dependencies
    Upgraded dependencies.
    Upgraded node.
    Moved from Yarn 1 to PNPM.
    Removed analytics.
    Replaced custom CI with GH Actions.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const TelegramBot = require('node-telegram-bot-api');
-const analytics = require('./src/analytics');
 const { initHandlers } = require('./src/handlers');
 const CONFIG = require('./src/config');
 
@@ -8,4 +7,4 @@ const URL = `${telegram.host}:${telegram.port}/bot${token}`;
 
 const bot = new TelegramBot(token, settings);
 bot.setWebHook(URL);
-initHandlers(bot, analytics);
+initHandlers(bot);
--- src/handlers.js
+++ src/handlers.js
@@ -7,8 +7,8 @@ More event type are described in official API of `node-telegram-bot-api`
 https://github.com/yagop/node-telegram-bot-api/blob/master/doc/usage.md
 */
 
-function createHandler(bot, query, track) {
-  const { regexp, reply, title } = query;
+function createHandler(bot, query) {
+  const { regexp, reply } = query;
 
   bot.onText(regexp, (msg, match) => {
     try {
@@ -17,14 +17,13 @@ function createHandler(bot, query, track) {
       const response = reply(notation) || error;
       const options = createOptions(msg);
       bot.sendMessage(id, response, options);
-      track(msg, title);
     } catch (e) {
       console.error(e);
     }
   });
 }
 
-function createInlineHandler(bot, track) {
+function createInlineHandler(bot) {
   const { createInlineArticles } = inline;
 
   bot.on('inline_query', msg => {
@@ -37,24 +36,15 @@ function createInlineHandler(bot, track) {
       console.error(e);
     }
   });
-
-  bot.on('chosen_inline_result', msg => {
-    try {
-      track(msg);
-    } catch (e) {
-      console.error(e);
-    }
-  });
 }
 
-function initHandlers(bot, analytics) {
-  const { track, trackInline } = analytics;
-  createInlineHandler(bot, trackInline);
-  createHandler(bot, roll, track);
-  createHandler(bot, full, track);
-  createHandler(bot, random, track);
-  createHandler(bot, help, track);
-  createHandler(bot, deprecated, track);
+function initHandlers(bot) {
+  createInlineHandler(bot);
+  createHandler(bot, roll);
+  createHandler(bot, full);
+  createHandler(bot, random);
+  createHandler(bot, help);
+  createHandler(bot, deprecated);
 }
 
 module.exports = {
